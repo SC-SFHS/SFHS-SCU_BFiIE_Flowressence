2f5bf4cb56bafa2ffe2c04900e148b92dea90e90
aditya2604 <aditya26042005@gmail.com>
2022-03-22 18:00:26 -0700

    followed Barnett video and fixed navigation

---
+++ Assets/CharacterMovementHelper.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.XR.CoreUtils;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class CharacterMovementHelper : MonoBehaviour
+{
+    private XROrigin XROrigin;
+    private CharacterController CharacterController;
+    private CharacterControllerDriver driver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        XROrigin = GetComponent<XROrigin>();
+        CharacterController = GetComponent<CharacterController>();
+        driver = GetComponent<CharacterControllerDriver>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateCharacterController();
+    }
+
+    protected virtual void UpdateCharacterController()
+    {
+        if (XROrigin == null || CharacterController == null)
+            return;
+
+        var height = Mathf.Clamp(XROrigin.CameraInOriginSpaceHeight, driver.minHeight, driver.maxHeight);
+
+        Vector3 center = XROrigin.CameraInOriginSpacePos;
+        center.y = height / 2f + CharacterController.skinWidth;
+
+        CharacterController.height = height;
+        CharacterController.center = center;
+    }
+}
