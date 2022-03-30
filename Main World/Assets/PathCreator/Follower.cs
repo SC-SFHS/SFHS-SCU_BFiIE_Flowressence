ecc8afa2c4a3c6669d57299805114c63e69989a2
brianwu23 <brianwu@sfhs.com>
2022-03-29 21:05:31 -0700

    3.29

---
+++ Main World/Assets/PathCreator/Follower.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class Follower : MonoBehaviour
+{
+    public PathCreator pathCreator;
+    public float speed = 5;
+    float distanceTravelled;
+
+    void Update()
+    {
+        distanceTravelled += speed * Time.deltaTime;
+        transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
+    }
+}
