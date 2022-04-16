5e8e826b02e9eed9e941f0af65294f59cd5b87f8
aditya2604 <aditya26042005@gmail.com>
2022-04-15 17:55:40 -0700

    added child worm objects to each worm object clone

--- Assets/Scripts/WormInstantiator.cs
+++ Assets/Scripts/WormInstantiator.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class WormInstantiator : MonoBehaviour
 {
     public GameObject wormObject;
+    public GameObject wormChild1;
+    public GameObject wormChild2;
     public int amount = 1000;
+
+    private float xDistance, yDistance, zDistance;
 
     GameObject[] worms;
     // Start is called before the first frame update
@@ -18,10 +22,32 @@ public class WormInstantiator : MonoBehaviour
         for (int i = 1; i < amount; i++)
         {
             worms[i] = Instantiate(wormObject);
+            wormChild1 = Instantiate(wormObject);
+            wormChild2 = Instantiate(wormObject);
+
+            xDistance = worms[i - 1].transform.localPosition[0] - 1f;
+            yDistance = worms[i - 1].transform.localPosition[1];
+            zDistance = worms[i - 1].transform.localPosition[2] - Random.Range(-10f, 10f);
+
+            if (worms[i - 1].transform.localPosition[2] < 350)
+                zDistance = worms[i - 1].transform.localPosition[2] + Random.Range(5f, 10f);
+            if (worms[i - 1].transform.localPosition[2] > 400)
+                zDistance = worms[i - 1].transform.localPosition[2] - Random.Range(5f, 10f);
+
             worms[i].transform.localPosition = new Vector3(
-                worms[i-1].transform.localPosition[0] - Random.Range(0, 3),
-                worms[i-1].transform.localPosition[1],
-                worms[i-1].transform.localPosition[2] - Random.Range(-15, 15)
+                xDistance,
+                yDistance,
+                zDistance
+            );
+            wormChild1.transform.localPosition = new Vector3(
+                xDistance,
+                yDistance,
+                zDistance + 30f
+            );
+            wormChild2.transform.localPosition = new Vector3(
+                xDistance,
+                yDistance,
+                zDistance - 30f
             );
         }
     }
@@ -29,6 +55,6 @@ public class WormInstantiator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
