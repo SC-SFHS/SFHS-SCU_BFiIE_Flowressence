7a5c27f7c075cf19e7948e9508a38ec0452cfcbb
aditya2604 <aditya26042005@gmail.com>
2022-04-19 15:10:12 -0700

    added ray caster deactivation after game starts and repositioned UI screen in scripting

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.XR.CoreUtils;
-using PathCreation;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class TimeManager : MonoBehaviour
 {
--- Assets/Scripts/UIScreen.cs
+++ Assets/Scripts/UIScreen.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class UIScreen : MonoBehaviour
 {
@@ -10,11 +11,13 @@ public class UIScreen : MonoBehaviour
     public Text text;
     public TimeManager timeManager;
     public PathFollower pathFollower;
+    public XRInteractorLineVisual XRInteractorLeft;
+    public XRInteractorLineVisual XRInteractorRight;
 
     // Start is called before the first frame update
     void Start()
     {
-        canvas.transform.position = new Vector3(728f, 5f, pathFollower.pathCreator.path.GetPointAtDistance(-1f)[2]);
+        canvas.transform.position = new Vector3(pathFollower.pathCreator.path.GetPointAtDistance(-1f)[0] - 10f, pathFollower.pathCreator.path.GetPointAtDistance(-1f)[1] + 3f, pathFollower.pathCreator.path.GetPointAtDistance(-1f)[2] + 1f);
         button.onClick.AddListener(HideUIScreen);
     }
 
@@ -30,11 +33,18 @@ public class UIScreen : MonoBehaviour
         {
             canvas.SetActive(false);
             timeManager.gameStarted = true;
+            HideRayCaster();
         }
         else if (text.text == "EXIT")
         {
             // UnityEditor.EditorApplication.isPlaying = false; // <- exits out of play mode in Unity
             Application.Quit(); // <- quits the actual game, which can be tested after we build the game and run it
         }
+    }
+
+    void HideRayCaster()
+    {
+        XRInteractorLeft.gameObject.SetActive(false);
+        XRInteractorRight.gameObject.SetActive(false);
     }
 }
