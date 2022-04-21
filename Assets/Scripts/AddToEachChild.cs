e22e714c2785138d46e5f907d7a470a1c2fd92e0
NitishGourishetty <nitishgourishetty@gmail.com>
2022-04-20 22:33:35 -0700

    Added HRV

---
+++ Assets/Scripts/AddToEachChild.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddToEachChild : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.AddComponent<EmissionHRV>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
