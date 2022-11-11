4b2a0b4c6b2093f012f2068285c2db7f369a32c3
aditya2604 <aditya26042005@gmail.com>
2022-11-11 00:33:57 -0800

    tentatively final light manager

--- Assets/Scripts/LightManager.cs
+++ Assets/Scripts/LightManager.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.XR.CoreUtils;
 
-[System.Serializable] public class LightManager : MonoBehaviour
+public class LightManager : MonoBehaviour
 {
     public Light pointLight;
     public XROrigin rig;
 
+    [Range(-1, 1)]
     public float normHRDelta;
     public float normThreshHigh;
     public float normThreshLow;
@@ -23,7 +24,7 @@ using Unity.XR.CoreUtils;
     {
         pointLight.color = Color.white;
         pointLight.range = 50;
-        pointLight.intensity = 1;
+        pointLight.intensity = 0.5f;
 
         normThreshHigh = 0.05f;
         normThreshLow = -0.02f;
@@ -38,17 +39,35 @@ using Unity.XR.CoreUtils;
     void Update()
     {
         pointLight.transform.position = rig.transform.position;
+
         if (normHRDelta > normThreshHigh)
         {
-            blue = 0.0f;
-            red = k * normHRDelta;
+            if (changeColorTo(Color.red))
+                pointLight.intensity = k * normHRDelta + 0.5f;
         }
-        if (normHRDelta < normThreshLow)
+        else if (normHRDelta < normThreshLow)
         {
-            red = 0.0f;
-            blue = k * normHRDelta;
+            if (changeColorTo(Color.blue))
+                pointLight.intensity = k * -normHRDelta + 0.5f;
         }
+        else
+            changeColorTo(Color.white);
+    }
 
-        pointLight.color = new Color(red, green, blue);
+    private bool changeColorTo(Color color)
+    {
+        // first, fade intensity out to 0, at a rate proportional to the intensity (as intensity decreases, rate of change will also decrease)
+        if (pointLight.color != color && pointLight.intensity > 0)
+            pointLight.intensity = pointLight.intensity - pointLight.intensity * 0.01f - 0.01f;
+        // second, when the fade is complete (intensity = 0) set the point light's color to the desired color
+        if (pointLight.intensity == 0)
+            pointLight.color = color;
+        // third, once the new color has been set, raise the intensity back up
+        if (pointLight.color == color && pointLight.intensity < 0.5f)
+            pointLight.intensity += 0.01f;
+        // lastly, if the intensity has been maxed out for the new color, return true
+        if (pointLight.color == color && pointLight.intensity >= 0.5f)
+            return true;
+        return false;
     }
 }
