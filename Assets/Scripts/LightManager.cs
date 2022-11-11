0c6771cb81e7c86e62f247755367c4647716d993
ruhiyusuf <ruhi.yusuf@gmail.com>
2022-11-10 16:46:37 -0800

    add HR in light manager

--- Assets/Scripts/LightManager.cs
+++ Assets/Scripts/LightManager.cs
@@ -6,8 +6,17 @@ using Unity.XR.CoreUtils;
 [System.Serializable] public class LightManager : MonoBehaviour
 {
     public Light pointLight;
-    public bool update2Red, update2Green, update2Blue;
     public XROrigin rig;
+
+    public float normHRDelta;
+    public float normThreshHigh;
+    public float normThreshLow;
+
+    public float red;
+    public float green;
+    public float blue;
+
+    public float k;
 
     // Start is called before the first frame update
     void Start()
@@ -15,26 +24,31 @@ using Unity.XR.CoreUtils;
         pointLight.color = Color.white;
         pointLight.range = 50;
         pointLight.intensity = 1;
+
+        normThreshHigh = 0.05f;
+        normThreshLow = -0.02f;
+
+        red = green = blue = 0.0f;
+
+        // constant of proportionality
+        k = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
         pointLight.transform.position = rig.transform.position;
-        if (update2Red)
+        if (normHRDelta > normThreshHigh)
         {
-            pointLight.color = new Color(1.0f, 0.0f, 0.0f);
-            update2Red = false;
+            blue = 0.0f;
+            red = k * normHRDelta;
         }
-        if (update2Green)
+        if (normHRDelta < normThreshLow)
         {
-            pointLight.color = new Color(0.0f, 1.0f, 0.0f);
-            update2Green = false;
+            red = 0.0f;
+            blue = k * normHRDelta;
         }
-        if (update2Blue)
-        {
-            pointLight.color = new Color(0.0f, 0.0f, 1.0f);
-            update2Blue = false;
-        }
+
+        pointLight.color = new Color(red, green, blue);
     }
 }
